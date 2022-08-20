02d0b81c46680592495175b43510e3b567497f02
Antonio Serrano <antoniospcd2@gmail.com>
2022-08-20 13:56:35 -0500

    first commit

---
+++ src/common/pipes/parse-mongo-id-pipe.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ParseMongoIdPipe } from './parse-mongo-id-pipe.pipe';
+
+describe('ParseMongoIdPipePipe', () => {
+  it('should be defined', () => {
+    expect(new ParseMongoIdPipe()).toBeDefined();
+  });
+});
